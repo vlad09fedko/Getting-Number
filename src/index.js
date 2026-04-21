b89d9231e8b90262e7fabb9979e687d82e79c318
Vlad <vlad09fedko@gmaul.com>
2026-04-21 22:33:35 +0300

    Do app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function getNum(num) {
+  if (typeof num !== 'number' || Number.isNaN(num)) return NaN;
+  if (num < 1 || num > number || num === null) return null;
+  if (num === 1) return 1;
+
+  if (num !== null && getNum(num - 5) !== null) {
+    return `(${getNum(num - 5)} + 5)`;
+  }
+
+  if (num % 3 === 0 && getNum(num / 3) !== null) {
+    return `${getNum(num / 3)} * 3`;
+  }
+
+  return null;
+}
+
+const number = 13;
+const resultingEquation = getNum(number);
+if (Number.isNaN(resultingEquation)) {
+  console.log('Type error!');
+} else if (resultingEquation === null || resultingEquation === 1) {
+  console.log('Error!');
+} else {
+  console.log(`${number}: ${resultingEquation}`);
+}
